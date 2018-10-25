2898fe887a704efb36fe6d5ad01cf2d1e32d271c
CATIE GRASSE <CG184@duke.edu>
2018-10-24 22:43:41 -0400

    testing using statsmodels for regression

--- data_regression.py
+++ data_regression.py
@@ -2,6 +2,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.cross_validation import train_test_split
 import pandas as pd
 import numpy as np 
+import statsmodels as sm
 
 
 #input file should be a csv composed of feature and outcome columns with integer values
@@ -9,15 +10,15 @@ FILE_NAME = "testingProcessor.csv"
 
 
 #col_names = ["Male", "Female", "White", "Black or African American", "Other", "Readmission"]
-col_names = ["Age", "Sex", "Race", "Diabetes", "Readmission"]
+col_names = ["Age", "Sex", "Race", "Diabetes", "Smoking", "Dyspnea", "Hypertension", "Unplanned_Readmission", "Readmission"]
 dataset = pd.read_csv(FILE_NAME, header=None, names=col_names)
 
 #feature_cols = ["Male", "Female", "White", "Black or African American", "Other",]
-feature_cols = ["Age", "Sex", "Race", "Diabetes"]
+feature_cols = ["Age", "Sex", "Race", "Diabetes", "Smoking", "Dyspnea", "Hypertension"]
 
 
 X = dataset[feature_cols]
-y = dataset.Readmission
+y = dataset.Unplanned_Readmission
 
 #print sum([1 for num in y if num == 1])
 
@@ -28,13 +29,16 @@ X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.25, random_state=
 classifier = LogisticRegression(random_state=0)
 classifier.fit(X_train, y_train)
 
+# using stats models
+logit = sm.logit(y_train, X_train)
+results = logit.fit()
+
+print results.summary()
+
 # predict on the testing data
 y_pred = classifier.predict(X_test)
 
 # odds ratio 
 oddsRatio = np.exp(classifier.coef_)
 
-print "Prediction: \n", y_pred
-print "Test input: \n", X_test
-print "Expected output: \n", y_test
 print "Odds Ratio: \n", oddsRatio
